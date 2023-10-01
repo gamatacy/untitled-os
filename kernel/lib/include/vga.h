0abf283c7b49c5fd2b1af9b5254b13fb7ea401ae
OlegShipu <olegshipu95@gmail.com>
2023-10-01 18:15:04 +0300

    fix

--- kernel/lib/include/vga.h
+++ kernel/lib/include/vga.h
@@ -23,9 +23,6 @@ enum vga_colors{
 };
 void set_fg(enum vga_colors fg);
 void set_bg(enum vga_colors bg);
-struct vga_char make_char(char value, enum vga_colors fg, enum vga_colors bg);
-void putchar(char *c);
-void reverse(char* str, int n);
 void itoa(int num, char* str, int radix);
 void print(const char *string);
 void printf(const char* format, ...);
