bb3450b359e8b7ed5c67a1a26e282968c9e3c008
gamatacy <voroninivan003@gmail.com>
2023-10-14 13:33:31 +0300

    system loads, but paging is off idk why

--- kernel/main.c
+++ kernel/main.c
@@ -8,13 +8,10 @@ int kernel_main(){
     setup_idt();
     init_tty();
 
-    print("INITIALIZING PAGING");
-    init_paging();
-    print("PAGING INITIALIZED");
-
     for (uship8 i=0; i < TERMINALS_NUMBER; i++) {
         printf("TTY %d\n", i);
     }
+
     print("$ \n");
     while(1);
     return 0;
--- x86_64/boot/boot.asm
+++ x86_64/boot/boot.asm
@@ -25,19 +25,22 @@ page_tables_setup:
     mov eax, p2_table
     or eax, 0b11
     mov dword [p3_table + 0], eax
-    mov ecx, 0 ; counter
 
-.map_p2_table:
-    mov eax, 0x200000 ; 2MiB
+    mov eax, p1_table
+    or eax, 0b11
+    mov dword [p2_table + 0], eax
+
+    mov ecx, 0 ; counter
+.map_p1_table:
+    mov eax, 0x1000  ; Physical address for this page
     mul ecx
-    ; huge page bit
-    ; present bit - page is currently in memory
-    ; writable bit - page allowed to be written to
-    or eax, 0b1 << 7 | 0b11
-    mov [p2_table + ecx * 8], eax
+    or eax, 0b11       ; Set Present (P), Read/Write (R/W), and User (U/S) flags
+    mov [p1_table + ecx * 8], eax
     inc ecx
-    cmp ecx, 512
-    jne .map_p2_table
+    cmp ecx, 1024        ; 1024 entries in a page table
+    jl .map_p1_table
+
+
 
 .setup_page_register:
     ; move page table address to cr3
@@ -85,6 +88,8 @@ p3_table:
     resb 4096
 p2_table:
     resb 4096
+p1_table:
+    resb 4096
 stack_bottom:
     resb 4096*4
 stack_top:
@@ -108,5 +113,7 @@ section .text
 bits 64
 long_mode_start:
     mov rsp, stack_top
+
     call kernel_main
+
     hlt
