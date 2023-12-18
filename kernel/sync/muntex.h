1786d81c94ccf34cf40a83895605811d02a63677
OlegShipu <olegshipu95@gmail.com>
2023-12-18 23:51:43 +0300

    clean up

--- kernel/sync/muntex.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by ShipOS developers on 28.10.23.
-// Copyright (c) 2023 SHIPOS. All rights reserved.
-//
-
-#ifndef UNTITLED_OS_MUNTEX_H
-#define UNTITLED_OS_MUNTEX_H
-
-#endif //UNTITLED_OS_MUNTEX_H
